1ceb77291990f0cb0146cfb58a6151691140cc24
Michael Klos <meklos@outlook.com>
2020-02-07 23:09:37 -0500

    uwu

--- KozuBot.py
+++ KozuBot.py
@@ -14,4 +14,15 @@ async def ping(ctx):
 async def fuckshibi(ctx):
     await ctx.send("<:FuckShibi:632362814656479272>")
 
+@bot.event
+async def on_message(message):
+    if 'uwu' in message.content.casefold() and not message.author.bot:
+        await message.channel.send("NO UWU ON MY SERVER")
+        
+    if '0w0' in message.content.casefold() and not message.author.bot:
+        await message.channel.send("NO 0W0 ON MY SERVER")
+        
+    if 'owo' in message.content.casefold() and not message.author.bot:
+        await message.channel.send("NO OWO ON MY SERVER")
+        
 bot.run(token)
